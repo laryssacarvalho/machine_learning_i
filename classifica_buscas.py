5619584da87e706d7e6708800b041c1ffd882218
Laryssa Carvalho 2 <laryssaalves_carvalho@hotmail.com>
2019-01-18 16:41:53 -0200

    Algoritmo base

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import pandas as pd
 
 #data frame
@@ -34,14 +35,16 @@ modelo = MultinomialNB()
 modelo.fit(treino_dados, treino_marcacoes)
 
 resultado = modelo.predict(teste_dados)
-diferencas = resultado - teste_marcacoes
+acertos = (resultado == teste_marcacoes)
 
-acertos = [d for d in diferencas if d == 0]
-
-total_de_acertos = len(acertos)
+total_de_acertos = sum(acertos)
 total_de_elementos = len(teste_dados)
 
 taxa_de_acerto = 100 * total_de_acertos/total_de_elementos
-print(taxa_de_acerto)
+print("Taxa de acerto do algoritmo: %f:" % taxa_de_acerto)
 print(total_de_elementos)
 
+#a eficacia do algoritomo que chuta tudo 0 ou 1
+acerto_base = max(Counter(teste_marcacoes).values())#elemento mais comum de todos
+taxa_de_acerto_base = 100.0 * acerto_base/len(teste_marcacoes)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
